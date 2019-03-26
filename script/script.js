039266d9c79be4f9a8ba14cb76acc4070bc2f563
Fercemon <38923003+Fercemon@users.noreply.github.com>
2019-03-26 12:30:58 +0100

    slideShow

--- script/script.js
+++ script/script.js
@@ -115,6 +115,44 @@ function scrollFunction() {
 }
 
 
+
+// slideshow
+
+//open model
+document.querySelector(".gallery-btn").addEventListener("click", () => {
+    document.querySelector(".model").style.display = "block";
+});
+
+//close model
+document.querySelector(".close").addEventListener("click", () => {
+    document.querySelector(".model").style.display = "none";
+});
+
+
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
 /*
 function setupStuffs() {
 
